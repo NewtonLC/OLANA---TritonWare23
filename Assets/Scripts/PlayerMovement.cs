8a0bdb3be8f09c3aa775dc6bf0d083e543237afc
DrFreshPotato <khovandrew@gmail.com>
2023-10-11 20:37:49 -0700

    Made movement variables public
    Easier to manipulate
    Also doubled the speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,33 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 direction;
     private float playerSpeed = 0;
-    private const int MAX_PLAYER_SPEED = 5;
+    public float MAX_PLAYER_SPEED = 10.0f;
+    public float slowRate = 0.5f;
     private KeyCode[] MovementKeys = {KeyCode.W,KeyCode.A,KeyCode.S,KeyCode.D};
 
     void FixedUpdate()
     {
-        //If a movement key is pressed, set speed to MAX_PLAYER_SPEED.
-        //If a movement key is not pressed, then for each frame, lower player speed.
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
-
+        //Code that lowers speed if no movement keys are pressed, so the player stops smoothly.
+        if(is_Movement_Key_Pressed()){
+            playerSpeed = MAX_PLAYER_SPEED;
         }
+        else{
+            playerSpeed -= slowRate;
+        }
+
+        //Code that causes movement
         direction = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
         direction.Normalize();
         transform.Translate(direction*Time.deltaTime*playerSpeed);
+
+        //TODO: Add sword-swinging functionality, if player clicks then run sword anim
     }
 
     bool is_Movement_Key_Pressed(){
         foreach(KeyCode movementKey in MovementKeys){
-
+            if(Input.GetKey(movementKey)){
+                return true;
+            }
         }
         return false;
     }
