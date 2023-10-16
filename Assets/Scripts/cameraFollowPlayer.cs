6b56defbb2ef051ae53f331100504ef991e384b9
DrFreshPotato <khovandrew@gmail.com>
2023-10-15 22:56:13 -0700

    SmoothCameraFollow and Bounds

--- Assets/Scripts/cameraFollowPlayer.cs
+++ Assets/Scripts/cameraFollowPlayer.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class cameraFollowPlayer : MonoBehaviour
 {
-    public Camera mainCam; 
-    // Start is called before the first frame update
+    public Camera mainCam;
+    public Transform player;
+    //Ensures camera is on a different layer than game
+    public Vector3 offset = new Vector3(0,0,-1);
+    public Vector3 minBound, maxBound;
+    [Range(1,5)]
+    public float followSpeed;
+
     void Start()
     {
-   
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+        transform.position = player.position;
         
     }
+
+    void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 playerPos = player.position + offset;
+        Vector3 boundPos = new Vector3(
+            Mathf.Clamp(playerPos.x, minBound.x, maxBound.x),
+            Mathf.Clamp(playerPos.y, minBound.y, maxBound.y),
+            Mathf.Clamp(playerPos.z, -1, -10));
+
+        transform.position = Vector3.Lerp(transform.position, boundPos, followSpeed * Time.deltaTime);
+
+    }
 }
+
